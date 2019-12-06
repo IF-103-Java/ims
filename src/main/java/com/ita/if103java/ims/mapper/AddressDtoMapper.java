465c324aec6c5329304456624c95492f33e0113e
BohdanAndrusiak <bogdan.andrusiak@gmail.com>
2019-12-06 12:50:36 +0200

    Fix AssociateDtoMapper, AddressDtoMapper

--- src/main/java/com/ita/if103java/ims/mapper/AddressDtoMapper.java
+++ src/main/java/com/ita/if103java/ims/mapper/AddressDtoMapper.java
@@ -5,39 +5,41 @@ import com.ita.if103java.ims.entity.Address;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AddressDtoMapper {
+public class AddressDtoMapper extends AbstractEntityDtoMapper<Address, AddressDto> {
 
-    public AddressDto convertAddressToAddressDto(Address address) {
-        if (address == null) {
-            return null;
-        } else {
-            AddressDto addressDto = new AddressDto();
-            addressDto.setId(address.getId());
-            addressDto.setCountry(address.getCountry());
-            addressDto.setCity(address.getCity());
-            addressDto.setAddress(address.getAddress());
-            addressDto.setZip(address.getZip());
-            addressDto.setLatitude(address.getLatitude());
-            addressDto.setLongitude(address.getLongitude());
-
-            return addressDto;
-        }
-    }
-
-    public Address convertAddressDtoToAddress(AddressDto addressDto) {
-        if (addressDto == null) {
+    @Override
+    public Address toEntity(AddressDto dto) {
+        if (dto == null) {
             return null;
         } else {
             Address address = new Address();
-            address.setId(addressDto.getId());
-            address.setCountry(addressDto.getCountry());
-            address.setCity(addressDto.getCity());
-            address.setAddress(addressDto.getAddress());
-            address.setZip(addressDto.getZip());
-            address.setLatitude(addressDto.getLatitude());
-            address.setLongitude(addressDto.getLongitude());
+            address.setId(dto.getId());
+            address.setCountry(dto.getCountry());
+            address.setCity(dto.getCity());
+            address.setAddress(dto.getAddress());
+            address.setZip(dto.getZip());
+            address.setLatitude(dto.getLatitude());
+            address.setLongitude(dto.getLongitude());
 
             return address;
+        }
+    }
+
+    @Override
+    public AddressDto toDto(Address entity) {
+        if (entity == null) {
+            return null;
+        } else {
+            AddressDto addressDto = new AddressDto();
+            addressDto.setId(entity.getId());
+            addressDto.setCountry(entity.getCountry());
+            addressDto.setCity(entity.getCity());
+            addressDto.setAddress(entity.getAddress());
+            addressDto.setZip(entity.getZip());
+            addressDto.setLatitude(entity.getLatitude());
+            addressDto.setLongitude(entity.getLongitude());
+
+            return addressDto;
         }
     }
 }
