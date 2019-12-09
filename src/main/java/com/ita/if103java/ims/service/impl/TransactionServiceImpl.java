cd2f4d8e0a4bdbbc2dd6fbc57d5edba5309e02aa
Rostislav Ivankiv <bbwgd77@gmail.com>
2019-12-09 16:48:57 +0200

    Add TransactionService

---
+++ src/main/java/com/ita/if103java/ims/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,53 @@
+package com.ita.if103java.ims.service.impl;
+
+import com.ita.if103java.ims.dao.TransactionDao;
+import com.ita.if103java.ims.dto.TransactionDto;
+import com.ita.if103java.ims.entity.Transaction;
+import com.ita.if103java.ims.entity.TransactionType;
+import com.ita.if103java.ims.service.AccountService;
+import com.ita.if103java.ims.service.AssociateService;
+import com.ita.if103java.ims.service.TransactionService;
+import com.ita.if103java.ims.service.UserService;
+import com.ita.if103java.ims.service.WarehouseService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+    private TransactionDao transactionDao;
+    private AccountService accountService;
+    private UserService userService;
+    //    private ItemService itemService;
+    private WarehouseService warehouseService;
+    private AssociateService associateService;
+
+    @Override
+    public TransactionDto findById(BigInteger id) {
+        final Transaction transaction = transactionDao.findById(id);
+        if (transaction.getType() == TransactionType.MOVE) {
+            return new TransactionDto(
+                transaction.getId(),
+                transaction.getTimestamp(),
+                transaction.getType(),
+                accountService.view(transaction.getAccountId()),
+                userService.findById(transaction.getWorkerId()),
+                null,
+                transaction.getQuantity(),
+                warehouseService.findWarehouseById(transaction.getMovedFrom()),
+                warehouseService.findWarehouseById(transaction.getMovedTo())
+            );
+        }
+        // TransactionType -> (IN, OUT)
+        return new TransactionDto(
+            transaction.getId(),
+            transaction.getTimestamp(),
+            transaction.getType(),
+            accountService.view(transaction.getAccountId()),
+            userService.findById(transaction.getWorkerId()),
+            null,
+            transaction.getQuantity(),
+            associateService.view(transaction.getAssociateId())
+        );
+    }
+}
