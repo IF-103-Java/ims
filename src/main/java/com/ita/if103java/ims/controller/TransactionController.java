54116e5966b513713889ce77390e5b30f65f26de
Rostislav Ivankiv <bbwgd77@gmail.com>
2019-12-11 14:34:55 +0200

    Transactions management (#45)

---
+++ src/main/java/com/ita/if103java/ims/controller/TransactionController.java
@@ -0,0 +1,29 @@
+package com.ita.if103java.ims.controller;
+
+
+import com.ita.if103java.ims.dto.TransactionDto;
+import com.ita.if103java.ims.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    private TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/{id}")
+    public TransactionDto findById(@PathVariable("id") BigInteger id) {
+        return transactionService.findById(id);
+    }
+}
