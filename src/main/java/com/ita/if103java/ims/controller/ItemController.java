fcef9c572bc28d7c0d060ca4d9ff259a590d3e18
Назар <ngvozdyanchuk@gmail.com>
2019-12-16 20:05:21 +0200

    Merge branch 'dev' of https://github.com/IF-103-Java/ims into Nazar_controller
     Conflicts:
    update controller
    	src/main/java/com/ita/if103java/ims/service/ItemService.java
    add @CrossOrigin

--- src/main/java/com/ita/if103java/ims/controller/ItemController.java
+++ src/main/java/com/ita/if103java/ims/controller/ItemController.java
@@ -7,6 +7,7 @@ import com.ita.if103java.ims.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,9 +19,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping({"/items"})
+@RequestMapping("/items")
 public class ItemController {
     private ItemService itemService;
 
