6b1d5f6d9f39da4580b890756ba417da39a16e66
Назар <ngvozdyanchuk@gmail.com>
2019-12-16 11:26:32 +0200

    add controller

--- src/main/java/com/ita/if103java/ims/ImsApplication.java
+++ src/main/java/com/ita/if103java/ims/ImsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ImsApplication.class, args);
     }
