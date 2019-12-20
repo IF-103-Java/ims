a82f6bdfbed0affdefb7eb9759eee70dc2eff4f1
Rostislav Ivankiv <bbwgd77@gmail.com>
2019-12-21 01:06:15 +0200

    Add DistanceMatrixMapper

--- src/main/java/com/ita/if103java/ims/service/impl/DistanceMatrixServiceImpl.java
+++ src/main/java/com/ita/if103java/ims/service/impl/DistanceMatrixServiceImpl.java
@@ -1,6 +1,5 @@
 package com.ita.if103java.ims.service.impl;
 
-import com.google.common.collect.Lists;
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.errors.ApiException;
@@ -8,29 +7,30 @@ import com.google.maps.model.DistanceMatrix;
 import com.google.maps.model.TravelMode;
 import com.ita.if103java.ims.dto.AddressDto;
 import com.ita.if103java.ims.dto.WarehouseToAssociateDistanceDto;
+import com.ita.if103java.ims.mapper.DistanceMatrixMapper;
 import com.ita.if103java.ims.service.DistanceMatrixService;
 import com.ita.if103java.ims.util.ListUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class DistanceMatrixServiceImpl implements DistanceMatrixService {
     private final GeoApiContext apiContext;
+    private final DistanceMatrixMapper matrixMapper;
 
-    public DistanceMatrixServiceImpl(GeoApiContext apiContext) {
+    @Autowired
+    public DistanceMatrixServiceImpl(GeoApiContext apiContext, DistanceMatrixMapper matrixMapper) {
         this.apiContext = apiContext;
+        this.matrixMapper = matrixMapper;
     }
 
     @Override
     public List<WarehouseToAssociateDistanceDto> getDistances(List<AddressDto> warehouseAddresses,
                                                               List<AddressDto> supplierAddresses,
                                                               List<AddressDto> clientAddresses) {
-
         try {
             final List<AddressDto> associateAddresses = ListUtil.concat(supplierAddresses, clientAddresses);
             final DistanceMatrix distanceMatrix = DistanceMatrixApi
@@ -40,11 +40,7 @@ public class DistanceMatrixServiceImpl implements DistanceMatrixService {
                 .mode(TravelMode.DRIVING)
                 .language("en-US")
                 .await();
-            return ListUtil.zip(
-                Lists.cartesianProduct(Arrays.asList(warehouseAddresses, associateAddresses)),
-                Stream.of(distanceMatrix.rows).flatMap(x -> Stream.of(x.elements)).collect(Collectors.toList()),
-                (x, y) -> new WarehouseToAssociateDistanceDto(x.get(0), x.get(1), y.distance)
-            );
+            return matrixMapper.toDtoList(distanceMatrix, warehouseAddresses, associateAddresses);
         } catch (InterruptedException | ApiException | IOException e) {
             e.printStackTrace();
         }
