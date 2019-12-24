caa8dacc0838c152dd0e878ed1108019c631ee87
MarrMika <slobodjanmarija1@gmail.com>
2019-12-24 16:24:58 +0200

    add errorInfoDto

---
+++ src/main/java/com/ita/if103java/ims/dto/ErrorInfoDto.java
@@ -0,0 +1,27 @@
+package com.ita.if103java.ims.dto;
+
+public class ErrorInfoDto {
+    private int status;
+    private String message;
+
+    public ErrorInfoDto(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
