7f6842c42e67fca4290b22680b3d75d115ee6e7b
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2019-12-24 14:13:26 +0200

    Remove date from Event contructor

--- src/main/java/com/ita/if103java/ims/config/WebSocketConfig.java
+++ src/main/java/com/ita/if103java/ims/config/WebSocketConfig.java
@@ -12,8 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-        stompEndpointRegistry.addEndpoint("/ims-websocket")
-            .withSockJS();
+        stompEndpointRegistry.addEndpoint("/ims-websocket").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
