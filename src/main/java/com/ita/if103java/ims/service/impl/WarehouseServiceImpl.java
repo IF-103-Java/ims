9b56c8f01375d6776cfb9c1924a2e94c87a1a08f
Ivan Mykhalevych <mychalevytsh@gmail.com>
2019-12-28 23:36:35 +0200

    add Events and Address in service

--- src/main/java/com/ita/if103java/ims/service/impl/WarehouseServiceImpl.java
+++ src/main/java/com/ita/if103java/ims/service/impl/WarehouseServiceImpl.java
@@ -1,13 +1,18 @@
 package com.ita.if103java.ims.service.impl;
 
+import com.ita.if103java.ims.dao.AddressDao;
 import com.ita.if103java.ims.dao.WarehouseDao;
 import com.ita.if103java.ims.dto.WarehouseDto;
-import com.ita.if103java.ims.entity.User;
+import com.ita.if103java.ims.entity.Address;
+import com.ita.if103java.ims.entity.Event;
+import com.ita.if103java.ims.entity.EventName;
 import com.ita.if103java.ims.entity.Warehouse;
 import com.ita.if103java.ims.exception.MaxWarehouseDepthLimitReachedException;
 import com.ita.if103java.ims.exception.MaxWarehousesLimitReachedException;
+import com.ita.if103java.ims.mapper.AddressDtoMapper;
 import com.ita.if103java.ims.mapper.WarehouseDtoMapper;
 import com.ita.if103java.ims.security.UserDetailsImpl;
+import com.ita.if103java.ims.service.EventService;
 import com.ita.if103java.ims.service.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -19,11 +24,23 @@ import java.util.List;
 public class WarehouseServiceImpl implements WarehouseService {
     private WarehouseDao warehouseDao;
     private WarehouseDtoMapper warehouseDtoMapper;
+    private AddressDao addressDao;
+    private AddressDtoMapper addressDtoMapper;
+    private EventService eventService;
 
     @Autowired
-    public WarehouseServiceImpl(WarehouseDao warehouseDao, WarehouseDtoMapper warehouseDtoMapper) {
+    public WarehouseServiceImpl(WarehouseDao warehouseDao,
+                                WarehouseDtoMapper warehouseDtoMapper,
+                                AddressDao addressDao,
+                                AddressDtoMapper addressDtoMapper,
+                                EventService eventService
+    ) {
+
         this.warehouseDao = warehouseDao;
         this.warehouseDtoMapper = warehouseDtoMapper;
+        this.addressDao = addressDao;
+        this.addressDtoMapper = addressDtoMapper;
+        this.eventService = eventService;
     }
 
     @Override
@@ -33,7 +50,7 @@ public class WarehouseServiceImpl implements WarehouseService {
             int maxWarehouses = userDetails.getAccountType().getMaxWarehouses();
             int warehouseQuantity = warehouseDao.findQuantityOfWarehousesByAccountId(accountId);
             if (warehouseQuantity < maxWarehouses) {
-                return createNewWarehouse(warehouseDto);
+                return createNewWarehouse(warehouseDto, userDetails);
             } else {
                 throw new MaxWarehousesLimitReachedException("The maximum number of warehouses has been reached for this" +
                     "{accountId = " + accountId + "}");
@@ -43,17 +60,28 @@ public class WarehouseServiceImpl implements WarehouseService {
         Integer maxWarehouseDepth = userDetails.getAccountType().getMaxWarehouseDepth();
         int parentLevel = warehouseDao.findLevelByParentID(warehouseDto.getParentID());
         if (parentLevel + 1 < maxWarehouseDepth) {
-            return createNewWarehouse(warehouseDto);
+            return createNewWarehouse(warehouseDto,userDetails);
         } else {
             throw new MaxWarehouseDepthLimitReachedException("The maximum depth of warehouse's levels has been reached for this" +
                 "{accountId = " + accountId + "}");
         }
     }
 
-    private WarehouseDto createNewWarehouse(WarehouseDto warehouseDto) {
-        Warehouse warehouse = warehouseDtoMapper.toEntity(warehouseDto);
+    private WarehouseDto createNewWarehouse(WarehouseDto warehouseDto, UserDetailsImpl user) {
+        Warehouse warehouse = warehouseDao.create(warehouseDtoMapper.toEntity(warehouseDto));
+        Address address = addressDtoMapper.toEntity(warehouseDto.getAddressDto());
+        addressDao.createWarehouseAddress(warehouse.getId(),address);
+        EventName eventName = EventName.WAREHOUSE_CREATED;
+        createEvent(user, warehouse, eventName);
+
         return warehouseDtoMapper.toDto(warehouseDao.create(warehouse));
     }
+
+    //without addresses and events
+//    private WarehouseDto createNewWarehouse(WarehouseDto warehouseDto) {
+//        Warehouse warehouse = warehouseDtoMapper.toEntity(warehouseDto);
+//        return warehouseDtoMapper.toDto(warehouseDao.create(warehouse));
+//    }
 
     @Override
     public List<WarehouseDto> findAll(Pageable pageable, UserDetailsImpl user) {
@@ -75,11 +103,30 @@ public class WarehouseServiceImpl implements WarehouseService {
         Warehouse updatedWarehouse = warehouseDtoMapper.toEntity(warehouseDto);
         Warehouse dBWarehouse = warehouseDao.findById(updatedWarehouse.getId());
         updatedWarehouse.setActive(dBWarehouse.isActive());
+        EventName eventName = EventName.WAREHOUSE_EDITED;
+        createEvent(user, updatedWarehouse, eventName);
         return warehouseDtoMapper.toDto(warehouseDao.update(updatedWarehouse));
     }
 
     @Override
     public boolean softDelete(Long id, UserDetailsImpl user) {
-        return warehouseDao.softDelete(id);
+        Boolean isDelete = warehouseDao.softDelete(id) ;
+        if (isDelete){
+        EventName eventName = EventName.WAREHOUSE_REMOVED;
+        Warehouse warehouse = warehouseDao.findById(id);
+        createEvent(user, warehouse, eventName);
+        }
+        return isDelete;
+    }
+
+    private void createEvent(UserDetailsImpl user, Warehouse warehouse, EventName eventName) {
+        Event event = new Event();
+        event.setMessage(eventName.getLabel() + " Name : " +
+                         warehouse.getName() + "id : " + warehouse.getId());
+        event.setAccountId(user.getUser().getAccountId());
+        event.setAuthorId(user.getUser().getId());
+        event.setName(eventName);
+
+        eventService.create(event);
     }
 }
