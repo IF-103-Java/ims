ad35152018d6f546a9a21011df116df38ed95a9a
Назар <ngvozdyanchuk@gmail.com>
2019-12-28 13:01:10 +0200

    fix duplicate

--- src/main/java/com/ita/if103java/ims/controller/SavedItemController.java
+++ src/main/java/com/ita/if103java/ims/controller/SavedItemController.java
@@ -9,20 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/savedItems")
+@CrossOrigin("http://localhost:4200/")
 public class SavedItemController {
     private ItemService itemService;
 
