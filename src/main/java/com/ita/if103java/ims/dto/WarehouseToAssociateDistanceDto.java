75e39e7a440b751b6c60c3307cfbaf1ebedc897f
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-03 17:21:19 +0200

    Add status field in Dto, create DistanceMatrixService (#85)
    * Add status field to WarehouseToAssociateDistanceDto
    
    * Create and move some code into DistanceMatrixService

--- src/main/java/com/ita/if103java/ims/dto/WarehouseToAssociateDistanceDto.java
+++ src/main/java/com/ita/if103java/ims/dto/WarehouseToAssociateDistanceDto.java
@@ -1,24 +1,25 @@
 package com.ita.if103java.ims.dto;
 
 import com.google.maps.model.Distance;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.maps.model.DistanceMatrixElementStatus;
 
 public class WarehouseToAssociateDistanceDto {
     private WarehouseAddressDto warehouseAddress;
     private AssociateAddressDto associateAddress;
     private Distance distance;
+    private DistanceMatrixElementStatus status;
 
     public WarehouseToAssociateDistanceDto() {
     }
 
-
-    @Autowired
     public WarehouseToAssociateDistanceDto(WarehouseAddressDto warehouseAddress,
                                            AssociateAddressDto associateAddress,
-                                           Distance distance) {
+                                           Distance distance,
+                                           DistanceMatrixElementStatus status) {
         this.warehouseAddress = warehouseAddress;
         this.associateAddress = associateAddress;
         this.distance = distance;
+        this.status = status;
     }
 
     public WarehouseAddressDto getWarehouseAddress() {
@@ -45,12 +46,21 @@ public class WarehouseToAssociateDistanceDto {
         this.distance = distance;
     }
 
+    public DistanceMatrixElementStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DistanceMatrixElementStatus status) {
+        this.status = status;
+    }
+
     @Override
     public String toString() {
         return "WarehouseToAssociateDistanceDto{" +
             "warehouseAddress=" + warehouseAddress +
             ", associateAddress=" + associateAddress +
-            ", distance=" + distance.humanReadable +
+            ", distance=" + distance +
+            ", status=" + status +
             '}';
     }
 }
