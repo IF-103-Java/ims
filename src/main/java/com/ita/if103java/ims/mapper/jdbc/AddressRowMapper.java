988252f70fb89f2d4d60476b54630930614a11d4
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-04 18:21:28 +0200

    Remove setValueOrNull from TransactionRowMapper, AddressRowMapper

--- src/main/java/com/ita/if103java/ims/mapper/jdbc/AddressRowMapper.java
+++ src/main/java/com/ita/if103java/ims/mapper/jdbc/AddressRowMapper.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.function.Consumer;
+
+import static com.ita.if103java.ims.util.RowMapperUtil.setValueOrNull;
 
 @Component
 public class AddressRowMapper implements RowMapper<Address> {
@@ -23,9 +24,5 @@ public class AddressRowMapper implements RowMapper<Address> {
         setValueOrNull(address::setWarehouseId, resultSet.getLong("warehouse_id"), resultSet);
         setValueOrNull(address::setAssociateId, resultSet.getLong("associate_id"), resultSet);
         return address;
-    }
-
-    private <T> void setValueOrNull(Consumer<T> consumer, T value, ResultSet rs) throws SQLException {
-        consumer.accept(rs.wasNull() ? null : value);
     }
 }
