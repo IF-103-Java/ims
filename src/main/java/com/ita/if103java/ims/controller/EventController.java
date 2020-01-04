d4aebfd44b7163c19195b733e08d2a98f31c8794
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2020-01-04 17:37:35 +0200

    EventController returns Page

--- src/main/java/com/ita/if103java/ims/controller/EventController.java
+++ src/main/java/com/ita/if103java/ims/controller/EventController.java
@@ -4,18 +4,20 @@ import com.ita.if103java.ims.dto.EventDto;
 import com.ita.if103java.ims.security.UserDetailsImpl;
 import com.ita.if103java.ims.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/events")
+@CrossOrigin("http://localhost:4200")
 public class EventController {
     private EventService eventService;
 
@@ -25,7 +27,7 @@ public class EventController {
     }
 
     @PostMapping
-    public List<EventDto> findAll(Pageable pageable, @RequestBody Map<String, ?> params, @AuthenticationPrincipal UserDetailsImpl user) {
+    public Page<EventDto> findAll(Pageable pageable, @RequestBody Map<String, ?> params, @AuthenticationPrincipal UserDetailsImpl user) {
         return eventService.findAll(pageable, params, user);
     }
 }
