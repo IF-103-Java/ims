0d00a2e8e4a778f3b678b96b15e398d0c8e9cfc4
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-08 18:08:52 +0200

    Update GlobalExceptionHandler, add ResponseMessageDto (#88)

--- src/main/java/com/ita/if103java/ims/config/GlobalExceptionHandler.java
+++ src/main/java/com/ita/if103java/ims/config/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.ita.if103java.ims.config;
 
-import com.ita.if103java.ims.dto.PesponseWrapperDto;
+import com.ita.if103java.ims.dto.handler.ResponseMessageDto;
 import com.ita.if103java.ims.exception.CRUDException;
 import com.ita.if103java.ims.exception.EntityNotFoundException;
 import com.ita.if103java.ims.exception.GoogleAPIException;
@@ -8,6 +8,7 @@ import com.ita.if103java.ims.exception.UserOrPasswordIncorrectException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,21 +18,26 @@ public class GlobalExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler({EntityNotFoundException.class})
-    public PesponseWrapperDto handleEntityNotFoundException(Exception e) {
+    public ResponseEntity<ResponseMessageDto> handleEntityNotFoundException(Exception e) {
         LOGGER.warn(e.getMessage(), e);
-        return new PesponseWrapperDto(HttpStatus.NOT_FOUND.value(), e.getMessage());
-    }
-
-    @ExceptionHandler({CRUDException.class, GoogleAPIException.class})
-    public PesponseWrapperDto handleCRUDException(Exception e) {
-        LOGGER.error(e.getMessage(), e);
-        return new PesponseWrapperDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(new ResponseMessageDto(e.getMessage()));
     }
 
     @ExceptionHandler({UserOrPasswordIncorrectException.class})
-    public PesponseWrapperDto handleUserOrPasswordIncorrectException(Exception e) {
+    public ResponseEntity<ResponseMessageDto> handleUserOrPasswordIncorrectException(Exception e) {
         LOGGER.warn(e.getMessage(), e);
-        return new PesponseWrapperDto(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
+        return ResponseEntity
+            .status(HttpStatus.UNAUTHORIZED)
+            .body(new ResponseMessageDto(e.getMessage()));
     }
 
+    @ExceptionHandler({CRUDException.class, GoogleAPIException.class})
+    public ResponseEntity<ResponseMessageDto> handleServerErrorExceptions(Exception e) {
+        LOGGER.error(e.getMessage(), e);
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ResponseMessageDto(e.getMessage()));
+    }
 }
