410796f2532434bd93ea9f3437d21a6d055053b7
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-10 19:38:52 +0200

    Move `create` method from TransactionService to TransactionDao (#109)

--- src/main/java/com/ita/if103java/ims/dao/TransactionDao.java
+++ src/main/java/com/ita/if103java/ims/dao/TransactionDao.java
@@ -1,9 +1,17 @@
 package com.ita.if103java.ims.dao;
 
+import com.ita.if103java.ims.dto.ItemTransactionRequestDto;
 import com.ita.if103java.ims.entity.Transaction;
+import com.ita.if103java.ims.entity.TransactionType;
+import com.ita.if103java.ims.entity.User;
 
 public interface TransactionDao {
     Transaction create(Transaction transaction);
+
+    Transaction create(ItemTransactionRequestDto itemTransactionRequestDto,
+                       User user,
+                       Long associateId,
+                       TransactionType type);
 
     Transaction findById(Long id);
 }
