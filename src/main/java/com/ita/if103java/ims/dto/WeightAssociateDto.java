21622155c3a78e81e3899f7a4296ca3e482e39e3
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-01-12 03:42:40 +0200

    Rename BestTradeDto to WeightAssociateDto

---
+++ src/main/java/com/ita/if103java/ims/dto/WeightAssociateDto.java
@@ -0,0 +1,43 @@
+package com.ita.if103java.ims.dto;
+
+public class WeightAssociateDto {
+    private Long associateId;
+    private Double weight;
+
+    public WeightAssociateDto() {
+    }
+
+    public WeightAssociateDto(Long associateId, Double weight) {
+        this.associateId = associateId;
+        this.weight = weight;
+    }
+
+    public Long getAssociateId() {
+        return associateId;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setAssociateId(Long associateId) {
+        this.associateId = associateId;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getReverseWeight() {
+        return 1.0 - getWeight();
+    }
+
+    @Override
+    public String toString() {
+        return "BenefitAssociateDto{" +
+            "associateId=" + associateId +
+            ", weight=" + weight +
+            ", reverseWeight=" + getReverseWeight() +
+            '}';
+    }
+}
