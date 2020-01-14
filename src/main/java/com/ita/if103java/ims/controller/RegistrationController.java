e103180f025c9792ff35731cd29c6882d409c892
MarrMika <slobodjanmarija1@gmail.com>
2020-01-14 14:55:29 +0200

    Add @Transactional, fix registration

--- src/main/java/com/ita/if103java/ims/controller/RegistrationController.java
+++ src/main/java/com/ita/if103java/ims/controller/RegistrationController.java
@@ -9,51 +9,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.ita.if103java.ims.config.MailMessagesConfig.ACTIVATE_USER;
-import static com.ita.if103java.ims.config.MailMessagesConfig.FOOTER;
-
 @RestController
 @RequestMapping("/signup")
 public class RegistrationController {
 
-    @Value("${mail.activationURL}")
-    private String activationURL;
-
     private UserService userService;
-    private MailService mailService;
-    private AccountService accountService;
 
     @Autowired
-    public RegistrationController(UserService userService,
-                                  AccountService accountService,
-                                  MailService mailService) {
+    public RegistrationController(UserService userService) {
         this.userService = userService;
-        this.accountService = accountService;
-        this.mailService = mailService;
     }
 
     @PostMapping(
         produces = MediaType.APPLICATION_JSON_VALUE,
         consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public void createUser(@Validated({NewData.class}) @RequestBody UserDto userDto) {
-        UserDto createdUser = userService.create(userDto);
-        accountService.create(createdUser, userDto.getAccountName());
-        String token = createdUser.getEmailUUID();
-        String message = "" +
-            ACTIVATE_USER
-            + activationURL + token + "\n" +
-            FOOTER;
-        mailService.sendMessage(createdUser, message, "ACCOUNT ACTIVATION");
-
+    public UserDto createUser(@Validated({NewData.class}) @RequestBody UserDto userDto) {
+        return userService.createAndSendMessage(userDto);
     }
 
 }
