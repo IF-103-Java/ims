b59b3718fa7e6e265d2ebc34a437442a3f8619a4
BohdanAndrusiak <bogdan.andrusiak@gmail.com>
2020-01-15 17:52:54 +0200

    Fix imports

--- src/main/java/com/ita/if103java/ims/service/impl/WarehouseItemAdviceServiceImpl.java
+++ src/main/java/com/ita/if103java/ims/service/impl/WarehouseItemAdviceServiceImpl.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
