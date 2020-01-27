0570b5cc1ae8e68842a5bc6982c8d7f5f4516688
MarrMika <slobodjanmarija1@gmail.com>
2020-01-27 13:13:57 +0200

    Fix imports

--- src/main/java/com/ita/if103java/ims/controller/LoginController.java
+++ src/main/java/com/ita/if103java/ims/controller/LoginController.java
@@ -9,7 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
@@ -27,7 +32,7 @@ public class LoginController {
         return loginService.signIn(userLoginDto);
     }
 
-    @PostMapping(value = "/forgot-password")
+    @PostMapping("/forgot-password")
     @ResponseStatus(HttpStatus.OK)
     public void forgotPassword(@Validated({NewData.class}) @RequestBody ForgotPasswordDto forgotPasswordDto) {
         loginService.sendResetPasswordToken(forgotPasswordDto);
