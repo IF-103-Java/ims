f10c09a4d95a7f8ad37a79bf3f771836e45a1bbc
Ivan Mykhalevych <mychalevytsh@gmail.com>
2020-01-28 01:20:19 +0200

    drop @CrossOrigin

--- src/main/java/com/ita/if103java/ims/controller/WarehouseController.java
+++ src/main/java/com/ita/if103java/ims/controller/WarehouseController.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin("http://localhost:4200/")
 @RequestMapping("/warehouses")
 public class WarehouseController {
     private WarehouseService warehouseService;
