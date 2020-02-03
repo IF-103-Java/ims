da56ea7e540716c4cc2ebfe1131caa11f67e731a
Andrii Holoshivskii <pustoj0@gmail.com>
2020-02-03 18:37:19 +0300

    dashboard update

--- src/main/java/com/ita/if103java/ims/controller/DashboardController.java
+++ src/main/java/com/ita/if103java/ims/controller/DashboardController.java
@@ -8,6 +8,9 @@ import com.ita.if103java.ims.dto.WarehousePremiumStructDto;
 import com.ita.if103java.ims.security.UserDetailsImpl;
 import com.ita.if103java.ims.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,7 +20,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -33,6 +38,7 @@ public class DashboardController {
 
     @GetMapping(value = "/warehouseLoad",
         produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     public List<WarehouseLoadDto> getWarehouseLoad(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return dashboardService.getWarehouseLoad(userDetails.getUser().getAccountId());
     }
@@ -40,6 +46,7 @@ public class DashboardController {
     @PreAuthorize("hasAuthority('DEEP_WAREHOUSE_ANALYTICS')")
     @GetMapping(value = "/premiumLoad/{id}",
         produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     public WarehousePremiumStructDto getPreLoad(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                 @PathVariable Long id) {
         return dashboardService.getPreLoad(id, userDetails.getUser().getAccountId());
@@ -48,6 +55,7 @@ public class DashboardController {
     @PostMapping(value = "/popularityItems",
         consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     public List<PopularItemsDto> getPopularItems(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                  @RequestBody PopularItemsRequestDto popularItems) {
         return dashboardService.getPopularItems(popularItems, userDetails.getUser().getAccountId());
@@ -55,8 +63,9 @@ public class DashboardController {
 
     @GetMapping(value = "/endingItems",
         produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<EndingItemsDto> getEndingItems(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                               @RequestParam int minQuantity) {
-        return dashboardService.getEndingItems(minQuantity, userDetails.getUser().getAccountId());
+    @ResponseStatus(HttpStatus.OK)
+    public Page<EndingItemsDto> getEndingItems(@ApiIgnore Pageable pageable, @RequestParam int minQuantity,
+                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return dashboardService.getEndingItems(pageable, minQuantity, userDetails.getUser().getAccountId());
     }
 }
