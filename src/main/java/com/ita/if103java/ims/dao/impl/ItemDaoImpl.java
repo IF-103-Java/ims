9b2ee5e28438f43986dfd89a7c9b8fe5bb10523d
Hvozdianchuck <ngvozdyanchuk@gmail.com>
2020-02-04 21:50:02 +0200

    fix active

--- src/main/java/com/ita/if103java/ims/dao/impl/ItemDaoImpl.java
+++ src/main/java/com/ita/if103java/ims/dao/impl/ItemDaoImpl.java
@@ -226,7 +226,7 @@ public class ItemDaoImpl implements ItemDao {
         static final String SQL_SELECT_ITEMS_BY_ID_AND_ACCOUNT_ID = """
                 select *
                 from items
-                where account_id=? and %s
+                where account_id=? and active = true and %s
             """;
         static final String SQL_INSERT_INTO_ITEM = """
                 insert into items(name_item, unit, description, volume, active, account_id)
