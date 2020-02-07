d00287ce2d02fbe92b6a8f2106c01290c58b900d
Hvozdianchuck <ngvozdyanchuk@gmail.com>
2020-02-07 07:57:40 +0200

    fix dtoMappers

--- src/main/java/com/ita/if103java/ims/mapper/dto/SavedItemAssociateDtoMapper.java
+++ src/main/java/com/ita/if103java/ims/mapper/dto/SavedItemAssociateDtoMapper.java
@@ -5,9 +5,24 @@ import com.ita.if103java.ims.entity.Associate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SavedItemAssociateDtoMapper {
-   public SavedItemAssociateDto toDto(Associate associate){
-        if (associate == null){
+public class SavedItemAssociateDtoMapper extends AbstractEntityDtoMapper<Associate, SavedItemAssociateDto> {
+
+    @Override
+    public Associate toEntity(SavedItemAssociateDto dto) {
+        if (dto == null) {
+            return null;
+        }
+        Associate associate = new Associate();
+        associate.setId(dto.getId());
+        associate.setName(associate.getName());
+        associate.setEmail(associate.getEmail());
+        associate.setPhone(associate.getPhone());
+        return null;
+    }
+
+    @Override
+    public SavedItemAssociateDto toDto(Associate associate) {
+        if (associate == null) {
             return null;
         }
         SavedItemAssociateDto savedItemAssociateDto = new SavedItemAssociateDto();
