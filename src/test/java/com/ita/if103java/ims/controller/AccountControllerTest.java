960257e6e7b258bf9305a2c2868d91f893fefcd8
Dasha Boychuk <dashaboichuk@gmail.com>
2020-02-17 22:57:55 +0200

    tests

--- src/test/java/com/ita/if103java/ims/controller/AccountControllerTest.java
+++ src/test/java/com/ita/if103java/ims/controller/AccountControllerTest.java
@@ -2,8 +2,11 @@ package com.ita.if103java.ims.controller;
 
 import com.ita.if103java.ims.dto.AccountDto;
 import com.ita.if103java.ims.dto.UserDto;
+import com.ita.if103java.ims.entity.AccountType;
+import com.ita.if103java.ims.entity.Role;
 import com.ita.if103java.ims.entity.User;
 import com.ita.if103java.ims.handler.GlobalExceptionHandler;
+import com.ita.if103java.ims.security.UserDetailsImpl;
 import com.ita.if103java.ims.service.AccountService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Collections;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
@@ -43,6 +48,8 @@ class AccountControllerTest {
 
     private User user;
     private AccountDto accountDto;
+    private UserDetailsImpl userDetails;
+    private AccountType accountType;
 
     @BeforeEach
     void setUp() {
@@ -51,7 +58,22 @@ class AccountControllerTest {
 
         user = new User();
         user.setId(1L);
+        user.setFirstName("First name");
+        user.setLastName("Last name");
+        user.setPassword("nfdfsasf");
+        user.setEmail("im.user@gmail.com");
+        user.setEmailUUID("rddfgfd");
+        user.setUpdatedDate(ZonedDateTime.now(ZoneId.systemDefault()));
+        user.setUpdatedDate(ZonedDateTime.now(ZoneId.systemDefault()));
+        user.setRole(Role.ROLE_ADMIN);
+        user.setActive(true);
         user.setAccountId(2L);
+        accountType = new AccountType();
+        accountType.setId(1L);
+        accountType.setName("Basic");
+        accountType.setLevel(1);
+        accountType.setActive(true);
+        userDetails = new UserDetailsImpl(user, accountType);
 
         accountDto = new AccountDto(1L, "Name", 1L, true);
 
@@ -72,7 +94,7 @@ class AccountControllerTest {
 
     @Test
     void view() throws Exception {
-        when(accountService.view(user.getAccountId())).thenReturn(accountDto);
+        when(accountService.view(userDetails.getUser().getAccountId())).thenReturn(accountDto);
         mockMvc.perform(get("/accounts/")
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
