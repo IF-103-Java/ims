424fcc7d41ee1f6e12139a11c8955d8517853132
Dasha Boychuk <dashaboichuk@gmail.com>
2020-02-17 12:59:31 +0200

    init controller tests.

---
+++ src/test/java/com/ita/if103java/ims/controller/AccountControllerTest.java
@@ -0,0 +1,21 @@
+package com.ita.if103java.ims.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void view() {
+    }
+}
---
+++ src/test/java/com/ita/if103java/ims/controller/InvitationControllerTest.java
@@ -0,0 +1,17 @@
+package com.ita.if103java.ims.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvitationControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void invite() {
+    }
+}
